8bff903578f03004896fcb10b3f4e7c4337e1db7
Thang Le <jakethecat21@gmail.com>
2021-08-01 19:38:57 +0700

    sample components

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import Navbars from '../base/navbars/Navbars';
 import Jumbotrons from '../base/jumbotrons/Jumbotrons';
 import Collapses from '../base/collapses/Collapses';
+import ButtonGroups from '../buttons/button-groups/ButtonGroups';
+import AdvancedTables from '../tables/advanced-tables/AdvancedTables';
+import { RouteComponentProps } from 'react-router-dom';
 
-const Dashboard = () => {
+/*
+Anh em import và preview thêm về các component của CoreUI, sau này Backend có dữ liệu thì lắp thêm vào
+*/
+
+interface IDashboard extends RouteComponentProps {}
+
+const Dashboard = (props: IDashboard) => {
   return (
     <>
+      <AdvancedTables />
       <Navbars />
       <Jumbotrons />
       <Collapses />
+      <ButtonGroups />
     </>
   );
 };
